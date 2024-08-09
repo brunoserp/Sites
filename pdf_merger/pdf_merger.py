42a64f9f0125b30ec989f5415dbf749fda77c017
Bruno <107195623+brunoserp@users.noreply.github.com>
2024-08-09 11:45:13 -0300

    Add files via upload

--- pdf_merger/pdf_merger.py
+++ pdf_merger/pdf_merger.py
@@ -1,97 +1,64 @@
 import streamlit as st
-import os
 import PyPDF2
 import re
+import io
 
 # ___________________________________________________________________________________________________________________________________
 # Funções para mesclagem de PDFs
 # ___________________________________________________________________________________________________________________________________
-def todo_pdf(pasta, nome_arquivo="pdf_merged.pdf"):
-    merger = PyPDF2.PdfMerger()
-    for file in os.listdir(pasta):
-        if file.endswith('.pdf'):
-            caminho_pdf = os.path.join(pasta, file)
-            merger.append(caminho_pdf)
 
-    output_path = os.path.join(pasta, nome_arquivo)
-    
+def todo_pdf(uploaded_files, nome_arquivo="pdf_merged.pdf"):
+    merger = PyPDF2.PdfMerger()
+    for file in uploaded_files:
+        pdf_reader = PyPDF2.PdfReader(io.BytesIO(file.read()))
+        merger.append(pdf_reader)
+
+    output = io.BytesIO()
     try:
-        merger.write(output_path)
-        st.markdown(f'''
-            <div style="display: flex; justify-content: center; align-items: center; height: 100px;">
-                <p style="font-size: 16px; color: green; font-weight: bold;">PDFs mesclados salvos com sucesso em:<br>{output_path}</p>
-            </div>
-        ''', unsafe_allow_html=True)
+        merger.write(output)
+        output.seek(0)  # Reset the buffer position to the beginning
+        return output.getvalue()
     except Exception as e:
         st.error(f"Ocorreu um erro ao salvar o PDF: {e}")
     finally:
         merger.close()
 
-def mesclar_paginas(pasta, paginas, nome_arquivo="pdf_merged.pdf"):
-    paginas_indicadas = set()
-    
-    # Separa as páginas por vírgula e hífen
-    for parte in paginas.split(','):
-        if '-' in parte:
-            inicio, fim = parte.split('-')
-            paginas_indicadas.update(range(int(inicio), int(fim) + 1))
-        else:
-            paginas_indicadas.add(int(parte))
-
+def mesclar_pdfs(uploaded_files, paginas, nome_arquivo="pdf_merged.pdf"):
     merger = PyPDF2.PdfMerger()
-    pdfs_com_problema = []
-    problemas_resumidos = False
-    
-    for file in os.listdir(pasta):
-        if file.endswith('.pdf'):
-            caminho_pdf = os.path.join(pasta, file)
-            leitor_pdf = PyPDF2.PdfReader(caminho_pdf)
-            qtd_paginas = len(leitor_pdf.pages)
+    paginas = paginas.split(',')
+    for file in uploaded_files:
+        pdf_reader = PyPDF2.PdfReader(io.BytesIO(file.read()))
+        pdf_writer = PyPDF2.PdfWriter()
+        for pagina in paginas:
+            if '-' in pagina:
+                start, end = pagina.split('-')
+                for i in range(int(start), int(end) + 1):
+                    if i <= len(pdf_reader.pages):
+                        pdf_writer.add_page(pdf_reader.pages[i - 1])
+            else:
+                try:
+                    page_number = int(pagina)
+                    if page_number <= len(pdf_reader.pages):
+                        pdf_writer.add_page(pdf_reader.pages[page_number - 1])
+                except ValueError:
+                    st.error(f"O número da página '{pagina}' não é válido.")
+                    return
+        if pdf_writer.pages:
+            temp_output = io.BytesIO()
+            pdf_writer.write(temp_output)
+            temp_output.seek(0)
+            temp_pdf_reader = PyPDF2.PdfReader(temp_output)
+            merger.append(temp_pdf_reader)
 
-            # Verifica se as páginas indicadas estão dentro do alcance
-            paginas_fora_do_alcance = [pag for pag in paginas_indicadas if pag > qtd_paginas]
-
-            if paginas_fora_do_alcance:
-                pdfs_com_problema.append((file, qtd_paginas, paginas_fora_do_alcance))
-
-            # Filtra as páginas válidas para o PDF atual
-            paginas_validas = [pag - 1 for pag in paginas_indicadas if pag <= qtd_paginas]
-            if paginas_validas:
-                for pagina in paginas_validas:
-                    merger.append(caminho_pdf, pages=(pagina, pagina + 1))
-
-            # Checa se já existem mais de 5 PDFs com problemas
-            if len(pdfs_com_problema) > 5 and not problemas_resumidos:
-                st.warning("Há mais de 5 PDFs sem a(s) página(s) indicada(s). Abaixo estão as informações dos primeiros 5:")
-                problemas_resumidos = True
-
-    if pdfs_com_problema:
-        st.markdown('''
-            <div style="background-color: #ffeb3b; padding: 10px; border-radius: 5px; margin: 10px 0;">
-                <div style="display: flex; justify-content: flex-start; align-items: flex-start;">
-                    <p style="font-size: 16px; color: #333; font-weight: bold; text-align: left;">
-                        Os PDFs abaixo estão incompatíveis com as informações sinalizadas:<br>
-                        ''' + "<br>".join(f"O PDF '{pdf}' não tem as páginas {', '.join(map(str, paginas_fora_do_alcance))}. Ele vai até a página {qtd_paginas}." for pdf, qtd_paginas, paginas_fora_do_alcance in pdfs_com_problema[:5]) + '''
-                    </p>
-                </div>
-            </div>
-        ''', unsafe_allow_html=True)
-        st.warning("O PDF mesclado será salvo sem as páginas inexistentes indicadas acima.")
-    
-    # Salva o PDF mesclado
-    output_path = os.path.join(pasta, nome_arquivo)
+    output = io.BytesIO()
     try:
-        merger.write(output_path)
-        st.markdown(f'''
-            <div style="display: flex; justify-content: center; align-items: center; height: 100px;">
-                <p style="font-size: 16px; color: green; font-weight: bold;">PDFs mesclados salvos com sucesso em:<br>{output_path}</p>
-            </div>
-        ''', unsafe_allow_html=True)
+        merger.write(output)
+        output.seek(0)  # Reset the buffer position to the beginning
+        return output.getvalue()
     except Exception as e:
         st.error(f"Ocorreu um erro ao salvar o PDF: {e}")
     finally:
         merger.close()
-
 
 # ___________________________________________________________________________________________________________________________________
 # PROGRAMA PRINCIPAL
@@ -185,20 +152,27 @@ if opcoes == 'Juntar pelo menos uma página de todos os PDFs':
     ''', unsafe_allow_html=True)
     paginas = st.text_input("Páginas a serem mescladas: ")
 
-folder_path = st.text_input("Insira o caminho da pasta com os PDFs:")
-folder_path = os.path.normpath(folder_path)
+st.write("Faça o upload dos PDFs que você deseja mesclar:")
+uploaded_files = st.file_uploader("Escolha arquivos PDF", type="pdf", accept_multiple_files=True)
 
 if st.button("Mesclar"):
-    if folder_path and os.path.isdir(folder_path):
+    if uploaded_files:
         if opcoes == 'Juntar todas as páginas de todos os PDFs':
-            todo_pdf(folder_path)
+            pdf_data = todo_pdf(uploaded_files)
         elif opcoes == 'Juntar pelo menos uma página de todos os PDFs':
             if not re.fullmatch(r'[0-9,-]+', paginas):
                 st.write('A sequência de páginas que você informou não é compatível com as instruções. Reescreva-a corretamente.')
+                pdf_data = None
             else:
-                mesclar_paginas(folder_path, paginas)
+                pdf_data = mesclar_pdfs(uploaded_files, paginas)
+        if pdf_data:
+            st.download_button(
+                label="Baixar PDF Mesclado",
+                data=pdf_data,
+                file_name="pdf_merged.pdf",
+                mime="application/pdf"
+            )
     else:
-        st.write("Por favor, insira um caminho válido para a pasta.")
-
+        st.write("Por favor, faça o upload de pelo menos um arquivo PDF.")
 
 # streamlit run C:\Users\bserpellone\Desktop\Python\github\pdf_merger\pdf_merger.py
